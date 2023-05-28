fd85b34ca72ebd1757ae7a9c3588d2f001bef6fc
sathishsmpth <sathishsmpth@outlook.com>
2023-05-28 09:57:38 +0530

    upto data types

---
+++ 03_JavaScript/03_dataTypes/dataTypes.js
@@ -0,0 +1,13 @@
+let string = "FullStack Web Development";
+console.log(string.charCodeAt(3));
+
+let lastIndex = string.length - 1;
+console.log(string.charCodeAt(lastIndex));
+
+let firstName = "Sathish";
+
+console.log(firstName.split());
+console.log(firstName.split(""));
+
+let country = "India";
+console.log(country.substr(3, 4));
